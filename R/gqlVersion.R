bcac9b3bc68e8649f305d4e815004ebe6c822737
cafebedouin <cafebedouin@gmail.com>
2023-09-15 21:36:14 -0400

    Adding addressTransactions.R, currentHeight.R, getTokenCommon.R, getTokenId.R, getTokenName.R, gqlVersion.R, mempool.R, state.R, and token.R.

---
+++ R/gqlVersion.R
@@ -0,0 +1,52 @@
+gqlVersion <- function() {
+
+  #' addressTransactions
+  #'
+  #' @description Returns a list of transactionIds for a given address or list
+  #' of addresses.
+  #'
+  #' Uses an Ergo GraphQL instance to retrieve the following columns:
+  #' "transactionId", inclusionHeight", "timestamp"
+  #'
+  #' By using the description tag you'll notice that I
+  #' can have multiple paragraphs in the description section
+  #'
+  #' @param address character. This is the Ergo address variable.
+  #' @param y character. The second item to paste Defaults to "!" but
+  #' "?" would be pretty great too
+  #' @usage AddressRanking("address")
+  #' @return The inputs pasted together as a character string.
+  #' @details The inputs can be anything that can be input into
+  #' the paste function.
+  #' @note And here is a note. Isn't it nice?
+  #' @section I Must Warn You:
+  #' The reference provided is a good read.
+  #' \subsection{Other warning}{
+  #'   It is completely irrelevant to this function though.
+  #' }
+  #'
+  #' @references Tufte, E. R. (2001). The visual display of
+  #' quantitative information. Cheshire, Conn: Graphics Press.
+  #' @examples
+  #' mypaste(1, 3)
+  #' mypaste("hey", "you")
+  #' mypaste("single param")
+  #' @export
+  #' @importFrom base paste
+
+  source("./R/env.R")
+
+  qry <- Query$new()
+  qry$query('getversioninfo', 'query { info { version } }')
+
+  con <- GraphqlClient$new(gqlURL)
+  res <- con$exec(qry$queries$getversioninfo)
+  json <- as.list(jsonlite::fromJSON(res))
+  version <- as.character(json$data$info$version)
+
+  if (!is.null(version)) {
+    return(version)
+  } else {
+    return(NULL)
+  }
+}
---
+++ R/mempool.R
@@ -0,0 +1,54 @@
+mempool <- function() {
+
+  #' addressTransactions
+  #'
+  #' @description Returns a list of transactionIds for a given address or list
+  #' of addresses.
+  #'
+  #' Uses an Ergo GraphQL instance to retrieve the following columns:
+  #' "transactionId", inclusionHeight", "timestamp"
+  #'
+  #' By using the description tag you'll notice that I
+  #' can have multiple paragraphs in the description section
+  #'
+  #' @param address character. This is the Ergo address variable.
+  #' @param y character. The second item to paste Defaults to "!" but
+  #' "?" would be pretty great too
+  #' @usage AddressRanking("address")
+  #' @return The inputs pasted together as a character string.
+  #' @details The inputs can be anything that can be input into
+  #' the paste function.
+  #' @note And here is a note. Isn't it nice?
+  #' @section I Must Warn You:
+  #' The reference provided is a good read.
+  #' \subsection{Other warning}{
+  #'   It is completely irrelevant to this function though.
+  #' }
+  #'
+  #' @references Tufte, E. R. (2001). The visual display of
+  #' quantitative information. Cheshire, Conn: Graphics Press.
+  #' @examples
+  #' mypaste(1, 3)
+  #' mypaste("hey", "you")
+  #' mypaste("single param")
+  #' @export
+  #' @importFrom base paste
+
+  source("./R/env.R")
+
+  qry <- Query$new()
+  qry$query('getmempoolinfo', 'query
+            { mempool { size, transactionsCount } }')
+
+  con <- GraphqlClient$new(gqlURL)
+  res <- con$exec(qry$queries$getmemepoolinfo)
+  json <- as.data.frame(jsonlite::fromJSON(res))
+  View(json)
+  break
+
+  if (!is.null(mempool)) {
+    return(mempool)
+  } else {
+    return(NULL)
+  }
+}
